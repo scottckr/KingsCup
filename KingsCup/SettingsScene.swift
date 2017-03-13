38705350e990ae4dcb13ed56e8f6c8314ebcc489
scottckr <scottckr@hotmail.com>
2017-03-13 14:14:46 +0100

    New scenes.

---
+++ KingsCup/SettingsScene.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsScene.swift
+//  KingsCup
+//
+//  Created by Scott Crocker on 2017-03-13.
+//  Copyright © 2017 Scott Crocker. All rights reserved.
+//
+
+import SpriteKit
+
+class SettingsScene: SKScene {
+    
+}
